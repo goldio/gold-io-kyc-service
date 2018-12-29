bb7a7721a2d21b0bd4ca8a1bcf2b58afa4c41b9e
Profiteam <31069986+Profiteam@users.noreply.github.com>
2018-12-29 17:07:04 +0300

    base crud service interface

---
+++ Gold.Io.Kyc/Gold.IO.Kyc.Services/Interfaces/IBaseCrudService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.IO.Kyc.Services.Interfaces
+{
+    public interface IBaseCrudService<T>
+    {
+        IQueryable<T> GetAll();
+        T Get(long id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(T item);
+        void Delete(long id);
+    }
+}
