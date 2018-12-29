54101ec9a5ae126a606971bbc1ab088876959007
Profiteam <31069986+Profiteam@users.noreply.github.com>
2018-12-29 17:01:39 +0300

    base models

---
+++ Gold.Io.Kyc/Gold.Io.Kyc.Models/IDeletableObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.Io.Kyc.Models
+{
+    public interface IDeletableObject
+    {
+        bool Deleted { get; set; }
+    }
+}
---
+++ Gold.Io.Kyc/Gold.Io.Kyc.Models/PersistentObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.Io.Kyc.Models
+{
+    public abstract class PersistentObject : Entity<long>
+    {
+    }
+}
